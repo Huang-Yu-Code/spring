7d19a8057754ffd02cdab8e79ba6281a30ebafd1
黄宇 <13713507941@163.com>
2021-08-19 11:51:10 +0800

    spring

--- amqp/src/main/java/com/github/codingob/amqp/service/queue/QueueConsumer.java
+++ amqp/src/main/java/com/github/codingob/amqp/service/queue/QueueConsumer.java
@@ -1,8 +1,12 @@
 package com.github.codingob.amqp.service.queue;
 
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 /**
  * 简单队列(P2P)
@@ -13,9 +17,20 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class QueueConsumer {
-    @RabbitListener(queues = {"queue"})
+    @RabbitListener(queues = {"queue"}, ackMode = "MANUAL")
     @RabbitHandler
-    public void receive(String message) {
-        System.out.println("queue: " + message);
+    public void receive(String msg, Channel channel, Message message) throws IOException {
+        System.out.println("queue: " + msg);
+        System.out.println(channel);
+        System.out.println(message);
+        try {
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+        } catch (IOException e) {
+            e.printStackTrace();
+            // 拒绝确认消息
+            channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true);
+            // 拒绝消息
+//            channel.basicReject(message.getMessageProperties().getDeliveryTag(), true);
+        }
     }
 }
